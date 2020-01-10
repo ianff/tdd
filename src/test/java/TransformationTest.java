74b9c042ded027256e02835d6ce4c5932efaa4b5
qyfei <qyfei@thoughtworks.com>
2020-01-10 15:05:39 +0800

    return Whizz if the number is a multiple of 7

--- src/test/java/TransformationTest.java
+++ src/test/java/TransformationTest.java
@@ -17,4 +17,11 @@ class TransformationTest {
 
     assertEquals("Buzz", new Transformation().fizzBuzz(number));
   }
+
+  @Test
+  void should_return_whizz_given_a_number_is_multiple_of_7() {
+    int number = 14;
+
+    assertEquals("Whizz", new Transformation().fizzBuzz(number));
+  }
 }
