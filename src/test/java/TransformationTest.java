716304a7a8585b8f69b2808184cea85a85aaf88c
qyfei <qyfei@thoughtworks.com>
2020-01-10 15:04:53 +0800

    return Buzz if the number is a multiple of 5

--- src/test/java/TransformationTest.java
+++ src/test/java/TransformationTest.java
@@ -10,4 +10,11 @@ class TransformationTest {
 
     assertEquals("Fizz", new Transformation().fizzBuzz(number));
   }
+
+  @Test
+  void should_return_buzz_given_a_number_is_multiple_of_5() {
+    int number = 10;
+
+    assertEquals("Buzz", new Transformation().fizzBuzz(number));
+  }
 }
