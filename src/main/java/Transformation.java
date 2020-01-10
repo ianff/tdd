be3d480aa0bc83bc10e7fc91835c07a44d5c51a0
qyfei <qyfei@thoughtworks.com>
2020-01-10 15:03:59 +0800

    return Fizz if the number is a multiple of 3

---
+++ src/main/java/Transformation.java
@@ -0,0 +1,8 @@
+public class Transformation {
+  public String fizzBuzz(int number) {
+    if(number % 3 == 0) {
+      return "Fizz";
+    }
+    return null;
+  }
+}
---
+++ src/test/java/TransformationTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TransformationTest {
+
+  @Test
+  void should_return_fizz_given_a_number_is_multiple_of_3() {
+    int number = 6;
+
+    assertEquals("Fizz", new Transformation().fizzBuzz(number));
+  }
+}
