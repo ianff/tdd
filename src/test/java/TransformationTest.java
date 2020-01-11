d93eada593e4001dd590e044ad7362cc4473465d
qyfei <qyfei@thoughtworks.com>
2020-01-11 10:45:36 +0800

    return cases that if the number is a multiple of several special numbers

--- src/test/java/TransformationTest.java
+++ src/test/java/TransformationTest.java
@@ -33,9 +33,30 @@ class TransformationTest {
   }
 
   @Test
-  void should_return_fizz_given_a_number_is_a_multiple_of_3_and_5() {
+  void should_return_fizzbuzz_given_a_number_is_a_multiple_of_3_and_5() {
     int number = 15;
 
     assertEquals("FizzBuzz", new Transformation().fizzBuzz(number));
+  }
+
+  @Test
+  void should_return_buzzwhizz_given_a_number_is_a_multiple_of_7_and_5() {
+    int number = 35;
+
+    assertEquals("BuzzWhizz", new Transformation().fizzBuzz(number));
+  }
+
+  @Test
+  void should_return_fizzwhizz_given_a_number_is_a_multiple_of_7_and_3() {
+    int number = 21;
+
+    assertEquals("FizzWhizz", new Transformation().fizzBuzz(number));
+  }
+
+  @Test
+  void should_return_fizzbuzzwhizz_given_a_number_is_a_multiple_of_7_and_3_and_5() {
+    int number = 105;
+
+    assertEquals("FizzBuzzWhizz", new Transformation().fizzBuzz(number));
   }
 }
